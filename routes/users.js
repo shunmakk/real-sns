165014b4bffb511f8d8cc8967e6b818eb107c601
shunma <shunmakajioka12@gmail.com>
2024-02-24 23:07:03 +0900

    ユーザー情報のcrud操作api

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { isObjectIdOrHexString } = require("mongoose");
 const { resolve } = require("path");
 const User = require("../models/User");
 
@@ -24,13 +25,107 @@ router.put("/:id" ,async(req, res)=> {
 })
 
 
-
-
 //ユーザー情報の削除
-
+router.delete("/:id" ,async(req, res)=> {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+      try{
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("ユーザー情報が削除されました")
+      }
+      catch(err){
+        return res.status(500).json(err);
+      }
+    }
+    else{
+        return res.status(403).json("あなたは自分のアカウントの情報のみ削除することができます")
+    }
+  })
 
 
 //ユーザー情報の取得
+router.get("/:id" ,async(req, res)=> {
+    
+      try{
+        const user = await User.findById(req.params.id);
+
+        //パスワードとupdateatを第三者に見られないようにする
+        const {password, updatedAt, ...others} = user._doc;
+
+        res.status(200).json(others);
+      }
+      catch(err){
+        return res.status(500).json(err);
+      }
+  })
+
+
+  //ユーザーのフォロー
+  router.put("/:id/follow" , async (req, res) => {
+      if(req.body.userId !== req.params.id){
+        try{
+        //相手のユーザーの情報を探す
+         const user = await User.findById(req.params.id);
+         const currentUser = await User.findById(req.body.userId);
+         //フォロワーに自分がいなかったらフォローできる
+         if(!user.followers.includes(req.body.userId)){
+            await user.updateOne({
+                $push: {
+                    followers: req.body.userId,
+                }
+            })
+            await currentUser.updateOne({
+                $push : {
+                    followings: req.params.id,
+                }
+            });
+            return res.status(200).json('フォローに成功しました！')
+        } else {
+          return  res.status(403).json('あなたはすでにこのユーザーをフォローしています')
+        }
+        }
+        catch(err){
+         return res.status(500).json(err);
+        }
+
+      }
+      else{
+          return res.status(500).json("自分のことはフォローできません")
+      }
+  })
+
+  //ユーザーのフォローを外す
+  router.put("/:id/unfollow" , async (req, res) => {
+    if(req.body.userId !== req.params.id){
+      try{
+      //相手のユーザーの情報を探す
+       const user = await User.findById(req.params.id);
+       const currentUser = await User.findById(req.body.userId);
+       //フォロワーにいたらフォロワーを探す
+       if(user.followers.includes(req.body.userId)){
+          await user.updateOne({
+              $pull: {
+                  followers: req.body.userId,
+              }
+          })
+          await currentUser.updateOne({
+              $pull : {
+                  followings: req.params.id,
+              }
+          });
+          return res.status(200).json('フォロー解除しました')
+      } else {
+        return  res.status(403).json('このユーザーはフォロー解除できません')
+      }
+      }
+      catch(err){
+       return res.status(500).json(err);
+      }
+
+    }
+    else{
+        return res.status(500).json("自分のことはフォロー解除できません")
+    }
+})
 
 
 
